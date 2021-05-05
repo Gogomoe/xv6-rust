ce9b4a5c600306354d599d3d3e2e5390797bc5cf
Gogo <gogo81745.moe@gmail.com>
2021-05-05 22:41:47 +0800

    add sleep lock

---
+++ src/sleep_lock.rs
@@ -0,0 +1,80 @@
+use alloc::string::String;
+use core::cell::{Cell, UnsafeCell};
+use core::ops::{Deref, DerefMut};
+
+use spin::Mutex;
+
+use crate::process::{CPU_MANAGER, PROCESS_MANAGER};
+
+pub struct SleepLock<T: ?Sized> {
+    lock: Mutex<()>,
+    locked: Cell<bool>,
+    name: String,
+    data: UnsafeCell<T>,
+}
+
+unsafe impl<T: ?Sized + Send> Sync for SleepLock<T> {}
+
+impl<T> SleepLock<T> {
+    pub const fn new(data: T, name: String) -> Self {
+        Self {
+            lock: Mutex::new(()),
+            locked: Cell::new(false),
+            name,
+            data: UnsafeCell::new(data),
+        }
+    }
+}
+
+impl<T: ?Sized> SleepLock<T> {
+    pub fn lock(&self) -> SleepLockGuard<'_, T> {
+        let mut guard = self.lock.lock();
+        while self.locked.get() {
+            CPU_MANAGER.my_cpu().sleep(self.locked.as_ptr() as usize, guard);
+            guard = self.lock.lock();
+        }
+        self.locked.set(true);
+        drop(guard);
+        SleepLockGuard {
+            lock: &self,
+            data: unsafe { &mut *self.data.get() },
+        }
+    }
+
+    fn unlock(&self) {
+        let guard = self.lock.lock();
+        self.locked.set(false);
+        self.wakeup();
+        drop(guard)
+    }
+
+    fn wakeup(&self) {
+        PROCESS_MANAGER.wakeup(self.locked.as_ptr() as usize);
+    }
+}
+
+pub struct SleepLockGuard<'a, T: ?Sized> {
+    lock: &'a SleepLock<T>,
+    data: &'a mut T,
+}
+
+impl<'a, T: ?Sized> Deref for SleepLockGuard<'a, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &*self.data
+    }
+}
+
+impl<'a, T: ?Sized> DerefMut for SleepLockGuard<'a, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut *self.data
+    }
+}
+
+impl<'a, T: ?Sized> Drop for SleepLockGuard<'a, T> {
+    /// The dropping of the SpinLockGuard will call spinlock's release_lock(),
+    /// through its reference to its original spinlock.
+    fn drop(&mut self) {
+        self.lock.unlock();
+    }
+}
